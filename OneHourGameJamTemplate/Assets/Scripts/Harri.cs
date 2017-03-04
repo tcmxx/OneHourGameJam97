0944f0f14271515263a7b8e6cb7480b8f113d125
maxiaoxiao1991 <maxiaoxiao1991@gmail.com>
2017-03-04 22:53:10 +0200

    sdfsdf

---
+++ OneHourGameJamTemplate/Assets/Scripts/Harri.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harri : MonoBehaviour {
+
+	public LayerMask doorMask;
+
+	public float maxV = 1;
+	Rigidbody2D rb;
+
+
+	void Awake(){
+		rb = GetComponent <Rigidbody2D> ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Move(float dir){
+		rb.velocity = Vector3.right*dir*maxV;
+	}
+
+	public void Enter(){
+		RaycastHit2D hit = Physics2D.Raycast (transform.position,Vector3.forward,100,doorMask);
+		Debug.Log ("Try");
+		if (hit.collider != null) {
+			GameController.gameController.Enter ();
+			Debug.Log ("Enter");
+		}
+	}
+
+}
---
+++ OneHourGameJamTemplate/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	Harri harri;
+
+	void Awake(){
+		harri = GetComponent <Harri> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		harri.Move (Input.GetAxis ("Horizontal"));
+		if (Input.GetButtonDown ("Enter")) {
+			harri.Enter ();
+		}
+	}
+}
